cb4084c2db0804087c2c6b2e092e4c05ee746cd2
Farrukh Naveed Anjum <anjum.farrukh@gmail.com>
2024-02-11 15:11:24 +0500

    Added the Front End with one Stocks Chart using Mock Data

--- investment_site/portfolio/urls.py
+++ investment_site/portfolio/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
 from .views import CompanyViewSet, DailyStockPriceViewSet
+from .views import stock_graph
+from .views import DailyStockPriceList
 
 router = DefaultRouter()
 router.register(r'companies', CompanyViewSet)
 router.register(r'daily-stock-prices', DailyStockPriceViewSet)
 
 urlpatterns = [
+    path('stock-graph', stock_graph, name='stock_graph'),
     path('', include(router.urls)),
+    path('stocks/<str:symbol>/', DailyStockPriceList.as_view(), name='daily-stock-prices'),
 ]
--- investment_site/portfolio/views.py
+++ investment_site/portfolio/views.py
@@ -1,13 +1,51 @@
 from rest_framework import viewsets, permissions
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
 from .models import Company, DailyStockPrice
 from .serializers import CompanySerializer, DailyStockPriceSerializer
+from django.shortcuts import render
+from rest_framework import generics
+from datetime import timedelta
+from django.utils import timezone
+
+class DailyStockPriceList(generics.ListAPIView):
+    serializer_class = DailyStockPriceSerializer
+
+    def get_queryset(self):
+        """
+        Optionally restricts the returned purchases to a given company,
+        by filtering against a `symbol` query parameter in the URL.
+        """
+        queryset = DailyStockPrice.objects.all()
+        symbol = self.kwargs['symbol']  # Assuming you're capturing 'symbol' from the URL
+        today = timezone.now().date()
+        thirty_days_ago = today - timedelta(days=30)
+
+        company = Company.objects.filter(symbol=symbol).first()
+        if company:
+            queryset = queryset.filter(company=company, date__range=[thirty_days_ago, today])
+        return queryset
 
 class CompanyViewSet(viewsets.ModelViewSet):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
-    permission_classes = [permissions.IsAuthenticated]
+
+    def get_permissions(self):
+        """
+        Instantiates and returns the list of permissions that this view requires.
+        """
+        if self.request.method == 'GET':
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
 
 class DailyStockPriceViewSet(viewsets.ModelViewSet):
     queryset = DailyStockPrice.objects.all()
     serializer_class = DailyStockPriceSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+def stock_graph(request):
+    return render(request, 'stock_price.html')
